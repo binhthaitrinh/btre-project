de22bfff6062e050a0cd951d3e4c3e7cd5c51a59
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-04-20 00:18:54 -0700

    refactor search form choices to dictionary

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Listing
 from django.shortcuts import get_object_or_404
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 # Create your views here.
 
@@ -34,4 +35,11 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+
+    return render(request, 'listings/search.html', context)
